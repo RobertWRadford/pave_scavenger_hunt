f88cdd4c07e003ff3ef549263674901d3b591960
Robert Radford <RobertR928@gmail.com>
2021-01-25 00:25:56 -0500

    database exchanged

--- pave_scavenger_hunt/pave/settings.py
+++ pave_scavenger_hunt/pave/settings.py
@@ -92,11 +92,14 @@ WSGI_APPLICATION = 'pave.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
     }
 }
 
